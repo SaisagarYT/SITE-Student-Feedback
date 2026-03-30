01128c6f84b99f15e62f8c3243ad094c8bf45cf0
SaisagarYT <saisagarsylada@gmail.com>
2026-03-30 22:29:33 +0530

    updated the admin auth

--- functions/index.js
+++ functions/index.js
@@ -6,7 +6,7 @@ const app = require("./src/app");
 setGlobalOptions({ maxInstances: 10 });
 
 const jwtSecret = defineSecret("HTTP_FEEDBACK_SECRET");
-
+// i diclared the secrets globally to access.....
 exports.feedback = onRequest(
   { region: "asia-southeast1", secrets: [jwtSecret] },
   (req, res) => {
