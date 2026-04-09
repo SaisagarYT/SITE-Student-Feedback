4b4a1bba361642e7d3e71aaf098779ef74e66f28
SaisagarYT <saisagarsylada@gmail.com>
2026-04-09 17:58:11 +0530

    deleted unnecessary folders and files

--- src/app/dev/course/page.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function CoursePage() {
-  return <div className="p-6">Course Page Content</div>;
-}
--- src/app/dev/faculty/page.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function FacultyPage() {
-  return <div className="p-6">Faculty Page Content</div>;
-}
